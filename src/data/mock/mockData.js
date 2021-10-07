23e85a54053d33a028511e45dee511d2c7241da0
Bohdan Pankovych <pankovychb@gmail.com>
2021-10-07 11:54:31 +0300

    add mock events

--- src/data/mock/mockData.js
+++ src/data/mock/mockData.js
@@ -1,10 +1,9 @@
-import { TypeButton } from '../constants/ButtonTypeColor';
-import img1 from './img1.png';
-import img2 from './img2.png';
-import img3 from './img3.png';
-import img4 from './img4.png';
-import img5 from './img5.png';
-
+import { TypeButton } from "../constants/ButtonTypeColor";
+import img1 from "./mockImg/img1.png";
+import img2 from "./mockImg/img2.png";
+import img3 from "./mockImg/img3.png";
+import img4 from "./mockImg/img4.png";
+import img5 from "./mockImg/img5.png";
 
 const mock = {
   creators: [
@@ -21,39 +20,41 @@ const mock = {
       value: "カヌー優子",
     },
   ],
-   destination: {
-     name: "共有先",
-     items: [{
-      text: "内部で共有する",
-      type: TypeButton.SHARE,
-    },
-    {
+  destination: {
+    name: "共有先",
+    items: [
+      {
+        text: "内部で共有する",
+        type: TypeButton.SHARE,
+      },
+      {
         text: "保護者と共有する",
         type: TypeButton.SHARE,
-    },
-    {
+      },
+      {
         text: "緊急連絡先（内部）",
         type: TypeButton.EMERGENCY,
-    },
-    {
+      },
+      {
         text: "緊急連絡先（保護者）",
         type: TypeButton.EMERGENCY,
-    },]
-   },
+      },
+    ],
+  },
   sharedPageChildren: {
     title: "子供",
     classes: [
       {
         class: "ひよこ (0)",
-        kids: ["佐藤","鈴木","高橋","田中	",]
+        kids: ["佐藤", "鈴木", "高橋", "田中	"],
       },
       {
         class: "あひる (1)",
-        kids: ["伊藤","山本","中村","小林",]
+        kids: ["伊藤", "山本", "中村", "小林"],
       },
       {
         class: "ひばり (2)",
-        kids: ["加藤","吉田","山田","佐々木",]
+        kids: ["加藤", "吉田", "山田", "佐々木"],
       },
     ],
   },
@@ -64,7 +65,17 @@ const mock = {
   sharedPage: [
     {
       title: "場所",
-      tags: ["保育室", "遊戯室", "給食室", "事務室", "トイレ", "廊下", "階段", "園庭", "園外"],
+      tags: [
+        "保育室",
+        "遊戯室",
+        "給食室",
+        "事務室",
+        "トイレ",
+        "廊下",
+        "階段",
+        "園庭",
+        "園外",
+      ],
     },
     {
       title: "3 つの視点",
@@ -76,66 +87,82 @@ const mock = {
     },
     {
       title: "10 の姿",
-      tags: ["健康", "自立心", "協同性", "規範", "社会", "思考力", "自然", "数・文字", "言葉", "表現"],
+      tags: [
+        "健康",
+        "自立心",
+        "協同性",
+        "規範",
+        "社会",
+        "思考力",
+        "自然",
+        "数・文字",
+        "言葉",
+        "表現",
+      ],
     },
   ],
   eventsList: [
     {
       id: 1,
       img: img1,
-      title: '橋本凛',
-      tagList: ['2018','いくつかのテキスト','文章'],
-      description: 'ますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります',
-      date: '2018',
-      month: 'January',
-      day: '4',
-      group: '2C',
+      title: "橋本凛",
+      tagList: ["2018", "いくつかのテキスト", "文章"],
+      description:
+        "ますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります",
+      date: "2018",
+      month: "January",
+      day: "4",
+      group: "2C",
     },
     {
       id: 2,
       img: img2,
-      title: '',
-      tagList: ['2017','一月','文章'],
-      description: 'テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります',
-      date: '2018',
-      month: 'January',
-      day: '6',
-      group: '2C',
+      title: "",
+      tagList: ["2017", "一月", "文章"],
+      description:
+        "テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります",
+      date: "2018",
+      month: "January",
+      day: "6",
+      group: "2C",
     },
     {
       id: 3,
       img: img3,
-      title: '',
-      tagList: ['2020','四月','文章'],
-      description: 'テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります',
-      date: '2020',
-      month: 'April',
-      day: '3',
-      group: '4C',
+      title: "",
+      tagList: ["2020", "四月", "文章"],
+      description:
+        "テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります",
+      date: "2020",
+      month: "April",
+      day: "3",
+      group: "4C",
     },
     {
       id: 4,
       img: img4,
-      title: '',
-      tagList: ['三月','2019','文章', '文章'],
-      description: 'テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります',
-      date: '2020',
-      month: 'April',
-      day: '2',
-      group: '4C',
+      title: "",
+      tagList: ["三月", "2019", "文章", "文章"],
+      description:
+        "テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります",
+      date: "2020",
+      month: "April",
+      day: "2",
+      group: "4C",
     },
     {
       id: 5,
       img: img5,
-      title: '',
-      tagList: ['一月','2019','文章'],
-      description: 'テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります',
-      date: '2019',
-      month: 'January',
-      day: '10',
-      group: '4C',
-    }
-  ]
+      title: "",
+      tagList: ["一月", "2019", "文章"],
+      description:
+        "テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります",
+      date: "2019",
+      month: "January",
+      day: "10",
+      group: "4C",
+    },
+  ],
 };
 
 export default mock;
